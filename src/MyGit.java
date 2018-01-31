8b65dbb13ed60afd7c70a9fbab27f93a4214a1a3
douglasluo <dankel@andrew.cmu.edu>
2018-01-31 14:24:34 -0500

    update date information
    add the date of 2000 days later

--- src/MyGit.java
+++ src/MyGit.java
@@ -1,5 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class MyGit {
     public static void myGit() {
+        Date date = new Date();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMMM d, yyyy");
+        System.out.println("Date: " + dateFormatter.format(date));
+        // 20,000 days later
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DATE, 20000);
+        System.out.println("After 20,000 days: " + dateFormatter.format(calendar.getTime()));
+        // your info
         System.out.println("dankel");
         System.out.println("I love Cloud Computing");
     }
