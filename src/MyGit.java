9224c5b146b1373cebf79a1a796b3be3f0995e80
douglasluo <dankel@andrew.cmu.edu>
2018-01-31 14:06:05 -0500

    add date information
    date is in a good format

--- src/MyGit.java
+++ src/MyGit.java
@@ -1,5 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MyGit {
     public static void myGit() {
+        Date date = new Date();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMMM d, yyyy");
+        System.out.println("Date: " + dateFormatter.format(date));
         System.out.println("dankel");
         System.out.println("I love Cloud Computing");
     }
