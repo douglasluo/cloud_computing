5dff4e9b09fc5c643534c6abab81d99c0ecdc924
douglasluo <dankel@andrew.cmu.edu>
2018-01-31 14:02:08 -0500

    my first try

---
+++ src/MyGit.java
@@ -0,0 +1,10 @@
+public class MyGit {
+    public static void myGit() {
+        System.out.println("dankel");
+        System.out.println("I love Cloud Computing");
+    }
+
+    public static void main(String[] args) {
+        myGit();
+    }
+}
