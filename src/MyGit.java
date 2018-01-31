3343d3c427398388d3988db8149741d81f255ec3
douglasluo <dankel@andrew.cmu.edu>
2018-01-31 14:40:11 -0500

    update date information
    add the date of 2000 days before

--- src/MyGit.java
+++ src/MyGit.java
@@ -12,6 +12,11 @@ public class MyGit {
         Calendar calendar = new GregorianCalendar();
         calendar.add(Calendar.DATE, 20000);
         System.out.println("After 20,000 days: " + dateFormatter.format(calendar.getTime()));
+        // 20,000 days before
+        Calendar calendarPast = new GregorianCalendar();
+        calendarPast.add(Calendar.DATE, -20000);
+        Date past = calendarPast.getTime();
+        System.out.println("Before 20,000 days: " + dateFormatter.format(past));
         // your info
         System.out.println("dankel");
         System.out.println("I love Cloud Computing");
